0b9814b26767b376ac574008ffa52d1e5f1ca4d2
MUHURTO62 <eyenin139@gmail.com>
2026-05-27 17:40:39 +0600

    fix: make backend package importable and use backend/manage.py in build script

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 set -o errexit
 
 echo "Running Django migrations..."
-python manage.py migrate --noinput
+python backend/manage.py migrate --noinput
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python backend/manage.py collectstatic --noinput
 
 echo "Build complete!"
